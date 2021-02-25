ac76e42328d24b890aecf35f6aa549f3268dedaa
sagar-gupta-cs17 <52529199+sagar-gupta-cs17@users.noreply.github.com>
2021-02-25 14:58:21 +0530

    Commitnow

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -70,55 +70,19 @@ class App extends Component {
     return this.setState({ viewCompleted: false });
   };
 
-  renderTabList = () => {
-    return (
-      <div className="nav nav-tabs">
-        <span
-          onClick={() => this.displayCompleted(true)}
-          className={this.state.viewCompleted ? "nav-link active" : "nav-link"}
-        >
-          Complete
-        </span>
-        <span
-          onClick={() => this.displayCompleted(false)}
-          className={this.state.viewCompleted ? "nav-link" : "nav-link active"}
-        >
-          Incomplete
-        </span>
-      </div>
-    );
-  };
-
   renderItems = () => {
-    const { viewCompleted } = this.state;
-    const newItems = this.state.todoList.filter(
-      (item) => item.completed === viewCompleted
-    );
+    const newItems = this.state.todoList;
 
     return newItems.map((item) => (
-      <li
-        key={item.id}
-        className="list-group-item d-flex justify-content-between align-items-center"
-      >
-        <span
-          className={`todo-title mr-2 ${
-            this.state.viewCompleted ? "completed-todo" : ""
-          }`}
-          title={item.description}
-        >
+      <li key={item.id}>
+        <span>
           {item.title}
         </span>
         <span>
-          <button
-            className="btn btn-secondary mr-2"
-            onClick={() => this.editItem(item)}
-          >
+          <button onClick={() => this.editItem(item)}>
             Edit
           </button>
-          <button
-            className="btn btn-danger"
-            onClick={() => this.handleDelete(item)}
-          >
+          <button onClick={() => this.handleDelete(item)}>
             Delete
           </button>
         </span>
@@ -128,26 +92,16 @@ class App extends Component {
 
   render() {
     return (
-      <main className="container">
-        <h1 className="text-white text-uppercase text-center my-4">Todo app</h1>
-        <div className="row">
-          <div className="col-md-6 col-sm-10 mx-auto p-0">
-            <div className="card p-3">
-              <div className="mb-4">
-                <button
-                  className="btn btn-primary"
-                  onClick={this.createItem}
-                >
-                  Add task
-                </button>
-              </div>
-              {this.renderTabList()}
-              <ul className="list-group list-group-flush border-top-0">
-                {this.renderItems()}
-              </ul>
-            </div>
+      <main>
+        <h1>Todo app</h1>
+        <div>
+          <div>
+            <button onClick={this.createItem}>Add task</button>
           </div>
-        </div>
+            <ul>
+              {this.renderItems()}
+            </ul>
+          </div>
         {this.state.modal ? (
           <Modal
             activeItem={this.state.activeItem}
